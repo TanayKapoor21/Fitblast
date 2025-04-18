15ceabc6b18b52d9a7de0a457548e9615fc797ea
Tanay <tanaykapoor21@gmail.com>
2025-04-18 14:40:09 +0530

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [CommentSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Index for user posts and sorting by date
+PostSchema.index({ user: 1, createdAt: -1 });
+
+module.exports = mongoose.model('Post', PostSchema);
